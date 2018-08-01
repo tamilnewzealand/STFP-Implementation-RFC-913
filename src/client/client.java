bbe951f2a0c45f424577a1922613cbffb3a10e95
சகாயன் சிற்சபேசன் <ssit662@aucklanduni.ac.nz>
2018-08-02 11:31:34 +1200

    Initial Commit

---
+++ src/client/client.java
@@ -0,0 +1,9 @@
+package client;
+
+public class client {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        // write your code here
+    }
+}
---
+++ src/server/server.java
@@ -0,0 +1,9 @@
+package server;
+
+public class server {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        // write your code here
+    }
+}
